55dcdef6ba7263b4190190ce8ee329fefb393a68
Ubuntu <ubuntu@ip-172-31-35-200.ap-southeast-2.compute.internal>
2026-03-22 16:28:22 +0000

    shell_script.sh

---
+++ shell_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#Author: Abhinav Kumar
+#
+#sub: print the number which is divisible by 3 & 5 but not divisible by 3*5 = 15
+#
+#Range: 1 to 100
+#
+#
+
+for i in {1..100}; do
+if ([ `expr $i % 3` == 0 ] || [ `expr $i % 5` == 0 ]) && [ `expr $i % 15` != 0 ];
+then
+	echo $i
+fi;
+done
